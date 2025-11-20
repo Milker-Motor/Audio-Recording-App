9feea3f9190e30e95316f708ee796a45f3bbf177
Oleksii Lytvynov-Bohdanov <o.lytvynovbogdanov@gmail.com>
2025-11-21 00:38:44 +0200

    Add new empty Xcode project

---
+++ Audio Recording App/Audio_Recording_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Audio_Recording_AppApp.swift
+//  Audio Recording App
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 21.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Audio_Recording_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
