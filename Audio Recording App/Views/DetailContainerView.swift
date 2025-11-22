885843ff5b0cd4109beff6b1172392b86720f7f1
Oleksii Lytvynov-Bohdanov <o.lytvynovbogdanov@gmail.com>
2025-11-22 23:38:41 +0200

    Add PlaceholderView as a default on detail view

--- Audio Recording App/Views/DetailContainerView.swift
+++ Audio Recording App/Views/DetailContainerView.swift
@@ -9,10 +9,9 @@ import SwiftUI
 
 struct DetailContainerView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Group {
+            PlaceholderView()
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
-}
-
-#Preview {
-    DetailContainerView()
 }
---
+++ Audio Recording App/Views/PlaceholderView.swift
@@ -0,0 +1,29 @@
+//
+//  PlaceholderView.swift
+//  Audio Recording App
+//
+//  Created by Oleksii Lytvynov-Bohdanov on 22.11.2025.
+//
+
+import SwiftUI
+
+struct PlaceholderView: View {
+    var body: some View {
+            VStack(spacing: 12) {
+                
+                Button(action: {
+                    print("Mic button tapped")
+                }) {
+                    Image(systemName: "mic.circle")
+                        .resizable()
+                        .frame(width: 72, height: 72)
+                        .foregroundStyle(.secondary)
+                }
+                .buttonStyle(PlainButtonStyle())
+                Text("Select a recording or start a new one")
+                    .font(.title3)
+                    .foregroundStyle(.secondary)
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+}
